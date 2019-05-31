800f05d772bbfb15b18edf55d0bb884b34422589
evi-hendrikx <36449832+evi-hendrikx@users.noreply.github.com>
2019-05-31 13:44:44 +0200

    Update pereira_runner.py

--- pereira_runner.py
+++ pereira_runner.py
@@ -1,7 +1,7 @@
 from analyses.classification import ClassifyStable, ClassifyROI, ClassifySearchlight
 from analyses.encoding import EncodingStable, EncodingROI, EncodingSearchlight
 
-user_dir = "/home/10978089/"
+user_dir = "/home/USERDIR/"
 
 # I chose index 1, 2, or 3, because these are also used in the reader.
 # They correspond to respectively "sentences", "pictures", "wordclouds"
@@ -12,8 +12,16 @@ for classification in (ClassifyROI(user_dir, paradigm), ClassifyStable(user_dir,
     
   
 for paradigm in [1,2,3]:
-    for encoding in (EncodingROI(user_dir, paradigm), EncodingStable(user_dir, paradigm), EncodingSearchlight(user_dir, paradigm)):
+    for encoding in (EncodingROI(user_dir, paradigm), EncodingStable(user_dir, paradigm)):
         accuracies = encoding.encode()
         print(accuracies.keys())
-
-
+                       
+                       
+for paradigm in [1,2,3]:
+    for clustering in (ClusterROI(user_dir, paradigm), ClusterStable(user_dir, paradigm)):
+        clusters = clustering.cluster_fmri()
+        
+for paradigm in [1,2,3]:
+    RSA = RSA_ROI_Stable(user_dir, paradigm)
+    correlations = RSA.run_RSA()
+    print(len(correlations))
