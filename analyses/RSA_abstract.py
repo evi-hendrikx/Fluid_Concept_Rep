7c61265fe10af0020c8d658df53513d0f380b47b
evi-hendrikx <36449832+evi-hendrikx@users.noreply.github.com>
2019-05-31 14:00:41 +0200

    Update RSA_abstract.py

--- analyses/RSA_abstract.py
+++ analyses/RSA_abstract.py
@@ -12,7 +12,7 @@ class RSA(object):
         self.paradigm = paradigm
         
         self.user_dir = user_dir
-        self.data_dir = "/datastore/pereira_data/"
+        self.data_dir = "DATADIR"
         self.subject_ids = [file for file in listdir(self.data_dir)]
         
         self.save_embedding_dir = user_dir + "embeddings/already_embedded/"
@@ -69,21 +69,10 @@ class RSA(object):
             self.correlations["abstract"] = data
         elif concreteness == "concrete":
             self.correlations["concrete"] = data
+        
             
-        if self.selection == "searchlight":
-            minimum = 2
-        else:
-            minimum = 2
-            
-        if len(self.correlations) == minimum:         
-            if self.selection == "searchlight":
-                if reduced_random == True:
-                    accuracy_file = self.save_dir + "RSA_" + paradigms[paradigm_index] + "_" + self.selection + "_reducedrandom.pickle"
-                else:
-                    accuracy_file = self.save_dir + "RSA_" + paradigms[paradigm_index] + "_" + self.selection + ".pickle"
-                    
-            else:
-                accuracy_file = self.save_dir + "RSA_" + paradigms[paradigm_index] + ".pickle"
+        if len(self.correlations) == 3:                   
+            accuracy_file = self.save_dir + "RSA_" + paradigms[paradigm_index] + ".pickle"
             
             os.makedirs(os.path.dirname(accuracy_file), exist_ok=True)
             with open(accuracy_file, 'wb') as handle:
@@ -108,19 +97,19 @@ class RSA(object):
             x[i] = np.asarray(data[i])
             
             # Calculate distances between vectors
-            print("Calculating Pearson for: " + labels[i] + " " + str(i))
+            print("Calculating cosine for: " + labels[i] + " " + str(i))
             C[i] = sp.spatial.distance.cdist(x[i], x[i], 'cosine')
     
 # =============================================================================
 #         for i in C:
 #             if not labels[i] == "RandomEncoder":
 #                 print("Start plotting")
-#                 self.plot(C[i], [], [x for x in range(1, len(C[i]+1))], labels[i], concreteness)
+#                 self.plot(C[i], [x for x in range(1, len(C[i]+1))], labels[i], concreteness)
 # =============================================================================
 
         return x, C
     
-    def plot(self, data, p_values, labels, title ="", concreteness = "", cbarlabel = "Pearson Distance", between_matrices = False):
+    def plot(self, data, labels, title ="", concreteness = "", cbarlabel = "Cosine", between_matrices = False):
         plt.rcParams["axes.grid"] = False
         plt.interactive(False)
     
@@ -142,10 +131,7 @@ class RSA(object):
         fig.tight_layout()
         
         paradigms = ["sentences", "pictures", "wordclouds", "average"]
-        if self.selection == "searchlight":
-            plot_name = self.save_dir + "RSA_" + paradigms[self.paradigm -1] + "_" + self.selection + "_" + concreteness + "_" + title + ".png" 
-        else:
-            plot_name = self.save_dir + "RSA_" + paradigms[self.paradigm -1] + "_roi_stable_" + concreteness + "_" + title + ".png" 
+        plot_name = self.save_dir + "RSA_" + paradigms[self.paradigm -1] + "_roi_stable_" + concreteness + "_" + title + ".png" 
             
         plt.savefig(plot_name)
         
@@ -209,21 +195,6 @@ class RSA(object):
         # what is happening here?
         ax.set_xticks(np.arange(0, data.shape[1] + 1) - 0.5, minor=True)
         ax.set_yticks(np.arange(0, data.shape[0] + 1) - 0.5, minor=True)
-        
-        if not p_values == []:
-            for i in range(data.shape[0]):
-                for j in range(data.shape[1]):
-                    if i == data.shape[0] - 1 or j == data.shape[1] - 1:
-                        continue
-                    else:
-                        if p_values[i][j] < 0.05:
-                            #ax.text(i, j, str(round(data[i][j], 2)) + "*", ha="center", va="center", color="w")
-                            ax.text(i, j, "*", ha="center", va="center", color="w")
-# =============================================================================
-#                         else:
-#                             ax.text(i, j, round(data[i][j], 2), ha="center", va="center", color="w")
-# 
-# =============================================================================
     
         return im, cbar
 
@@ -235,127 +206,45 @@ class RSA(object):
         # makes sure you only get average random in plot, but run accuracy against distribution
         randoms = labels.count("RandomEncoder")
         spearman = {}
-        spearman["p-values"] = np.zeros((len(keys) - randoms, len(keys) - randoms))
         spearman["labels"] = np.zeros((len(keys) - randoms, len(keys))).astype(str)
         random_cor = {}
         
-        if not self.selection == "searchlight":
-            spearman["correlations"] = np.zeros((len(keys) - randoms, len(keys)))
-            
-            # not compare the randoms against each other, but compare rest against all randoms
-            for i in np.arange(len(keys) - randoms):
-                random_cor[i] = []
-                for j in np.arange(len(keys)):
-                    print("Calculating distance between matrices: " + labels[i] + " and " + labels[j])
-                    corr_s = []
-                    for a, b in zip(C[keys[i]], C[keys[j]]):
-                        s, _ = sp.stats.spearmanr(a, b)
-                        corr_s.append(s)
-                    spearman["correlations"][i][j] = np.mean(corr_s)
-                    
-                    # save the correlations against random in variable somewhere
-                    if labels[j] == "RandomEncoder":
-                        random_cor[i].append(np.mean(corr_s))
-                        
-            for i in np.arange(len(keys) - randoms):
-                random_distribution = [spearman["correlations"][i][random_value] for random_value in range((len(keys) - randoms), len(keys))]
-                for j in np.arange(len(keys) - randoms):
-                    spearman["p-values"][i][j] = self.calculate_p_value(spearman["correlations"][i][j], random_distribution)
-                    
-            print(spearman)
-    
-            self.save_correlations(spearman, concreteness)
-            
-            # make plot with average random correlation instead of distribution
-            average_random_spearman = np.zeros((len(keys) - randoms + 1, len(keys) - randoms + 1))
-            for i in np.arange(len(keys) - randoms + 1):
-                for j in np.arange(len(keys) - randoms + 1):
-                    if j == len(keys) - randoms and i == len(keys) - randoms:
-                        average_random_spearman[i][j] = 1
-                    elif j == len(keys) - randoms:
-                        average_random_spearman[i][j] = np.mean(random_cor[i])
-                    elif i == len(keys) - randoms:
-                        average_random_spearman[i][j] = np.mean(random_cor[j])
-                    else:
-                        average_random_spearman[i][j] = spearman["correlations"][i][j]
-                    
-            labels = labels[:len(keys) - randoms + 1]
-            # average over the random ones
-            
-            
-            # self.plot(average_random_spearman, spearman["p-values"], labels, title="RDM Comparison Spearman", concreteness = concreteness, cbarlabel="Spearman Correlation", between_matrices = True)
-        
-        else:
-            spearman["correlations"] = np.zeros((len(keys) - randoms, len(keys) - randoms + 1))
-            done_subjects = []
-            for i in np.arange(len(keys) - randoms):
-                if "_" in labels[i]:
-                    subject_id = labels[i][:labels[i].index("_")]
-                    if not subject_id in done_subjects:
-                        done_subjects.append(subject_id)
-                        print("calculating spearman for subject " + subject_id)
-                    voxel_id = labels[i][labels[i].index("_")+ 1:]
-                    if voxel_id in self.random_voxels[subject_id]:
-                        random_cor[subject_id].append([])
-                        print("calculating random for voxel selection " + str(self.random_voxels[subject_id].index(voxel_id) + 1) + "/3")
-                        for j in np.arange(len(keys)):
-                            if labels[j] == "RandomEncoder":
-                                for a, b in zip(C[keys[i]], C[keys[j]]):
-                                    s, _ = sp.stats.spearmanr(a, b)
-                                    corr_s.append(s)
-                                random_cor[subject_id][self.random_voxels[subject_id].index(voxel_id)].append(np.mean(corr_s))
+        spearman["correlations"] = np.zeros((len(keys) - randoms, len(keys)))
 
-                for j in np.arange(len(keys)):
-                    spearman["labels"][i][j] = labels[i] + "_" + labels[j]
-                    if labels[j] == "PereiraEncoder" or labels[j] == "ImageEncoder" or labels[j] == "CombiEncoder":
-                        corr_s = []
-                        for a, b in zip(C[keys[i]], C[keys[j]]):
-                            if np.isnan(a).all() or np.isnan(b).all():
-                                corr_s.append(0)
-                            else:
-                                s, _ = sp.stats.spearmanr(a, b)
-                                corr_s.append(s)
-                        spearman["correlations"][i][j] = np.mean(corr_s)
-                    else:
-                        continue
-                        
-            random_distribution = {}
-            for i in np.arange(len(keys) - randoms):
-                if "_" in labels[i]:
-                    subject_id = labels[i][:labels[i].index("_")]
-                    if not subject_id in random_distribution:
-                        random_distribution[subject_id] = np.mean(random_cor[subject_id], axis = 0)
-                    spearman["correlations"][i][len(keys) - randoms] = np.mean(random_distribution[subject_id])
-                else:
-                    continue
-            
-            
-                
-            for i in np.arange(len(keys) - randoms):
-                if "_" in labels[i]:
-                    subject_id = labels[i][:labels[i].index("_")]
-                    for j in np.arange(len(keys)):
-                        if labels[j] == "PereiraEncoder" or labels[j] == "ImageEncoder" or labels[j] == "CombiEncoder":
-                            spearman["p-values"][i][j] = self.calculate_p_value(spearman["correlations"][i][j], random_distribution[subject_id])
-                else:
-                    continue
-                
-            print(spearman["correlations"])
-            print(spearman["p-values"])
-            print(spearman["labels"])
-            self.save_correlations(spearman, concreteness, reduced_random = True)
+        # not compare the randoms against each other, but compare rest against all randoms
+        for i in np.arange(len(keys) - randoms):
+            random_cor[i] = []
+            for j in np.arange(len(keys)):
+                print("Calculating distance between matrices: " + labels[i] + " and " + labels[j])
+                corr_s = []
+                for a, b in zip(C[keys[i]], C[keys[j]]):
+                    s, _ = sp.stats.spearmanr(a, b)
+                    corr_s.append(s)
+                spearman["correlations"][i][j] = np.mean(corr_s)
 
-            
-        
-    def calculate_p_value(self, accuracy, random):
-    
-        # adapted this from https://github.com/scikit-learn/scikit-learn/blob/7b136e92acf49d46251479b75c88cba632de1937/sklearn/model_selection/_validation.py
-        # best possible = 1/1001 --> 0.0099
-        n_permutations = len(random)
-        counter_random_better = 0
-        for random_score in random:
-            if random_score >= accuracy:
-                counter_random_better += 1
-        p_value = (counter_random_better + 1) / (n_permutations + 1)
-        
-        return p_value
+                # save the correlations against random in variable somewhere
+                if labels[j] == "RandomEncoder":
+                    random_cor[i].append(np.mean(corr_s))
+
+        print(spearman)
+
+        self.save_correlations(spearman, concreteness)
+
+        # make plot with average random correlation instead of distribution
+        average_random_spearman = np.zeros((len(keys) - randoms + 1, len(keys) - randoms + 1))
+        for i in np.arange(len(keys) - randoms + 1):
+            for j in np.arange(len(keys) - randoms + 1):
+                if j == len(keys) - randoms and i == len(keys) - randoms:
+                    average_random_spearman[i][j] = 1
+                elif j == len(keys) - randoms:
+                    average_random_spearman[i][j] = np.mean(random_cor[i])
+                elif i == len(keys) - randoms:
+                    average_random_spearman[i][j] = np.mean(random_cor[j])
+                else:
+                    average_random_spearman[i][j] = spearman["correlations"][i][j]
+
+        labels = labels[:len(keys) - randoms + 1]
+        # average over the random ones
+
+
+        self.plot(average_random_spearman, labels, title="RDM Comparison Spearman", concreteness = concreteness, cbarlabel="Spearman Correlation", between_matrices = True)
